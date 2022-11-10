149171338b7941061051bbc2c17a34275940bcbc
Kirillbiliashov <Kirillbeliashov14@gmail.com>
2022-11-10 11:46:45 +0200

    Connection pool class was added

---
+++ src/main/java/com/example/elective/ConnectionPool.java
@@ -0,0 +1,30 @@
+package com.example.elective;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionPool {
+
+  private static DataSource ds;
+
+  public static Connection getConnection() throws SQLException {
+    if (ds == null) configureDataSource();
+    return ds.getConnection();
+  }
+
+  private static void configureDataSource() {
+    try {
+      Context initContext = new InitialContext();
+      Context envContext  = (Context) initContext.lookup("java:/comp/env");
+      ds = (DataSource) envContext.lookup("jdbc/elective");
+    } catch (NamingException e) {
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
+  }
+
+}
